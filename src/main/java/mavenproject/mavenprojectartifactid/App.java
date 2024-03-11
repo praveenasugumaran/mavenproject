0c7b170c8b9cb97052a6a5e006e1e5d90d263c4e
praveena <itsmepraveena2000@gmail.com>
2024-03-11 17:59:47 +0530

    firstcommit

---
+++ src/main/java/mavenproject/mavenprojectartifactid/App.java
@@ -0,0 +1,13 @@
+package mavenproject.mavenprojectartifactid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
